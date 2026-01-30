04cff60af3430c8fee22fc132f43b2608a1aada2
Leo <leotuckercrispe@gmail.com>
2026-01-30 06:51:48 +0000

    Update fxmanifest.lua

--- LC_loading_V2/fxmanifest.lua
+++ LC_loading_V2/fxmanifest.lua
@@ -1,5 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
+author 'Leos customs'
+version '1.0.0'
+repository 'https://github.com/Leo0019-dev'
+description 'Custom loading screen'
 
 loadscreen 'index.html'
 
